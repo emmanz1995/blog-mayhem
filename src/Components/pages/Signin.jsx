211386a738825553c39b42a4f47b0a7dcb6e50d6
emmanz95 <emmanza2@gmail.com>
2021-03-31 18:34:44 +0100

    added animation

--- src/Components/pages/Signin.jsx
+++ src/Components/pages/Signin.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import '../scss/signin.scss';
 import AuthService from '../../Service/AuthService';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+
 
 const Signin = () => {
     const history = useHistory();
@@ -17,19 +20,21 @@ const Signin = () => {
     }
     return(
         <div className="signin-wrapper">
-            <div className="signin-opacity">
-                <img src="asset/react-logo.png" alt="" height="100" width="100"/>
-                <form className="signin-form">
-                    <h1>Sign In</h1>
-                    <input type="text" name="username" placeholder="Username" onChange={(evt) => setUsername(evt.target.value)} value={username} /><i className="fas fa-user fa-1x" /><br /><br />
-                    <input type="password" name="password" placeholder="Password" onChange={(evt) => setPassword(evt.target.value)} value={password} /><i className="fas fa-lock fa-1x" /><br /><br />
-                    <button onClick={signin}>Sign In</button>
-                    <br />
-                    <a href="">Forgot your Password, click me!</a>
-                </form>
-            </div>
+            <Zoom bottom>
+                <div className="signin-opacity">
+                    <img src="asset/react-logo.png" alt="" height="100" width="100"/>
+                    <form className="signin-form">
+                        <h1>Sign In</h1>
+                        <input type="text" name="username" placeholder="Username" onChange={(evt) => setUsername(evt.target.value)} value={username} /><i className="fas fa-user fa-1x" /><br /><br />
+                        <input type="password" name="password" placeholder="Password" onChange={(evt) => setPassword(evt.target.value)} value={password} /><i className="fas fa-lock fa-1x" /><br /><br />
+                        <button onClick={signin}>Sign In</button>
+                        <br />
+                        <a href="">Forgot your Password, click me!</a>
+                    </form>
+                </div>
+            </Zoom>
         </div>
-    )
-}
+    );
+};
 
 export default Signin;
